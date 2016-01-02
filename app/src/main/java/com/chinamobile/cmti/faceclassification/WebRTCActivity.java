bb8083cbb762e43e506f3668f8d080b9b5c10a2c
cmti95035 <cmti95035@gmail.com>
2016-01-02 15:14:52 -0800

    add more training picture

--- app/src/main/java/com/chinamobile/cmti/faceclassification/WebRTCActivity.java
+++ app/src/main/java/com/chinamobile/cmti/faceclassification/WebRTCActivity.java
@@ -2,14 +2,26 @@ package com.chinamobile.cmti.faceclassification;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.webkit.PermissionRequest;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
 
 public class WebRTCActivity extends AppCompatActivity {
     private WebView webView;
@@ -22,7 +34,31 @@ public class WebRTCActivity extends AppCompatActivity {
 
         webView = (WebView)findViewById(R.id.webview);
 
+        trustEveryone();
         startWebRTC();
+    }
+
+
+    private void trustEveryone() {
+        try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier(){
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }});
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[]{new X509TrustManager(){
+                public void checkClientTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {}
+                public void checkServerTrusted(X509Certificate[] chain,
+                                               String authType) throws CertificateException {}
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }}}, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(
+                    context.getSocketFactory());
+        } catch (Exception e) { // should never happen
+            e.printStackTrace();
+        }
     }
 
     private void startWebRTC() {
@@ -48,6 +84,11 @@ public class WebRTCActivity extends AppCompatActivity {
                 });
             }
 
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                Log.d(TAG, "received ssl error: " + error.toString());
+                handler.proceed(); // Ignore SSL certificate errors
+            }
         });
         try{
             Log.d(TAG, "will try to connect to https://ec2-52-53-232-49.us-west-1.compute.amazonaws.com:9001/");
